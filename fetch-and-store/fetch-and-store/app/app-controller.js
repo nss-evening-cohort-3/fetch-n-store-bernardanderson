dba901035d5fb86481f43bdd54e6865bd3398d25
Bernard Anderson <bernard.anderson@gmail.com>
2016-10-22 18:12:39 -0500

    Finished http query/Ajax

--- fetch-and-store/fetch-and-store/app/app-controller.js
+++ fetch-and-store/fetch-and-store/app/app-controller.js
@@ -1,10 +1,32 @@
 ﻿"use strict";
 
-app.controller("mainPage", function ($scope) {
+app.controller("mainPage", function ($scope, $http) {
 
-    $scope.updateQueryType = function (sentQueryType, sentInputURL) {
-        console.log(`Query Type updated to: ${sentInputURL}`);
-        $("#user-field").text(`You will ${sentQueryType} at ${sentInputURL}`);
+    $scope.inputURL = "httpstat.us/";
+
+    $scope.httpRequest = function (sentQueryType, sentInputURL) {
+        
+        // Current time before http request
+        let queryStart = Date.now();
+
+        // Sets the query report data 
+        let setResponseData = function (sentResponse) {
+            $scope.queryData = {
+                inputURL: sentResponse.config.url,
+                responseTime: Date.now() - queryStart, // Finds the difference between query received and query start
+                queryType: sentResponse.config.method,
+                statusCode: sentResponse.status,
+                responseSent: true
+            };
+        };
+
+        $http({
+            method: sentQueryType,
+            url: `http://${sentInputURL}`
+        }).then(function successCallback(response) {
+            setResponseData(response);
+        }, function errorCallback(response) {
+            setResponseData(response);
+        });
     }
-
 });
