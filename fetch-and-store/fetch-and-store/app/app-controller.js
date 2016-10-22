c1e514e1938c922a86e4093f6bb9cd3dc72a9d68
Bernard Anderson <bernardanderson@gmail.com>
2016-10-22 13:36:10 -0500

    angular form vars added and start of ajax

---
+++ fetch-and-store/fetch-and-store/app/app-controller.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+app.controller("mainPage", function ($scope) {
+
+    $scope.updateQueryType = function (sentQueryType, sentInputURL) {
+        console.log(`Query Type updated to: ${sentInputURL}`);
+        $("#user-field").text(`You will ${sentQueryType} at ${sentInputURL}`);
+    }
+
+});
