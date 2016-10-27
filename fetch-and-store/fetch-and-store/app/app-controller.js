b028c82335dd21fff7ab30d81a4fc6f85109c23c
Bernard Anderson <bernardanderson@gmail.com>
2016-10-26 21:04:39 -0500

    Added app.js minifying code

--- fetch-and-store/fetch-and-store/app/app-controller.js
+++ fetch-and-store/fetch-and-store/app/app-controller.js
@@ -1,5 +1,6 @@
 ﻿"use strict";
 
+// app.controller("mainPage", ['$scope', '$http', function ($scope, $http) { }]); // This is used in minifying 
 app.controller("mainPage", function ($scope, $http) {
 
     $scope.inputURL = "httpstat.us/";
